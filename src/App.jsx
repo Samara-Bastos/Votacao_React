d232ee9981a8b00c62e5d985bc3cf9b0ba57e5b8
Samara Bastos <samarabastos.dev@gmail.com>
2024-07-29 19:13:11 -0300

    Implementando o toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Cabecalho from './components/cabecalho';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
         <Cabecalho />
         <div className="Outlet">
+          <ToastContainer />
           <Outlet/>
         </div>
     </div>
