b46ad19f777e1c6ae2cf8558020d8f8f1bac2327
Samara Bastos <samarabastos.dev@gmail.com>
2024-07-30 16:21:24 -0300

    Implementado o métode de votar

--- src/pages/voto/Votacao.jsx
+++ src/pages/voto/Votacao.jsx
@@ -1,18 +1,54 @@
-import Botao from "../../components/botao";
 import Box from "../../components/card";
+import FormVoto from "../../components/formVoto";
 import TituloCard from "../../components/tituloCard";
+import Api from '../../lib/api';
 
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function Votacao(){
-    const teste = () => {
-        console.log("Botão clicado!");
-    };
+    const { id } = useParams();
+    const [Pauta, setPauta] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        Api.get(`/pauta/view/${id}`)
+            .then((response) => {
+                const data = response.data;
+                setPauta(data);
+            })
+            .catch((error) => {
+                toast.error('Ocorreu algum problema, tente novamente por favor!');
+            });
+    }, [id])
+
+    
+    const registrarVoto = ((e, FormDataVoto) => {
+        e.preventDefault();
+
+        Api.post(`/voto/${id}`, FormDataVoto)
+            .then((response) => {
+                toast.success('Voto inserido com sucesso!');
+                navigate('/pauta/view');
+            }).catch((error) => {
+                toast.error('Ocorreu algum problema, tente novamente por favor!');
+            })
+    })
     
     return(
        <Box>
-            <TituloCard texto={'Tela de votação'} color={'#607a8d'} />
-            <Botao texto={'NÃO'} onClick={teste} />
-            <Botao texto={'SIM'} onClick={teste} />
+            {
+                Pauta ?(
+                    <div> 
+                        <TituloCard texto={'Tela de votação'} color={'#607a8d'} />
+
+                        <FormVoto registrarVoto={registrarVoto} />
+                    </div>
+                ):(
+                    <span>A pauta não está disponivel no momento</span>
+                )
+            }
        </Box>
               
     )
